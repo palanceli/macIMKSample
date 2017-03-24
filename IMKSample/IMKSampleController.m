083cb497ebb0f89145998bf6fa02371b22e276ae
palance <palanceli@163.com>
2017-03-24 17:29:04 +0800

    commit changes

--- IMKSample/IMKSampleController.m
+++ IMKSample/IMKSampleController.m
@@ -45,34 +45,52 @@
 
 - (BOOL)handleEvent:(NSEvent *)event client:(id)sender
 {
-  NSLog(@"handleEvent:client: keyCode=0x%02hX", [event keyCode]);
+  NSLog(@"%@", event);
+  if([event type] == NSKeyDown){
+    unichar key = [[event characters] characterAtIndex:0];
+    // 如果是字符则追加到写作串
+    if((key >= 'a' && key <= 'z') ||(key >= '0' && key <= '9')){
+      [self appendComposedString:[event characters] client:sender];
+      return YES;
+    }
+    // 如果是空格或回车且有写作串则上屏
+    if(([event keyCode] == kVK_Space || [event keyCode] == kVK_Return)&&
+      [[self composedString] length] > 0)
+    {
+      [self commitComposedString:sender];
+      return YES;
+    }
+  }
   return NO;
 }
 
-- (BOOL)inputText:(NSString*)string
-              key:(NSInteger)keyCode
-        modifiers:(NSUInteger)flags
-           client:(id)sender
-{
-  NSLog(@"inputText:%@ key:0x%02lX modifiers:0x%04lX",
-        string, (long)keyCode, (unsigned long)flags);
-  
-  unichar key = [string characterAtIndex:0];
-  if((key >= 'a' && key <= 'z') || (key >= '0' && key <= '9'))
-  { // 如果是字符则追加到写作串
-    [self appendComposedString:string client:sender];
-    return YES;
-    
-  }
-  
-  if((keyCode == kVK_Space || keyCode == kVK_Return)
-     && [[self composedString] length]>0)
-  { // 如果是空格或回车且有写作串则上屏
-    [self commitComposedString:sender];
-    return YES;
-  }
-  return NO;
+- (NSUInteger)recognizedEvents:(id)sender {
+  return NSEventMaskFlagsChanged | NSEventMaskKeyDown | NSEventMaskKeyUp;
 }
+
+// - (BOOL)inputText:(NSString*)string
+//              key:(NSInteger)keyCode
+//        modifiers:(NSUInteger)flags
+//           client:(id)sender
+//{
+//  NSLog(@"inputText:%@ key:0x%02lX modifiers:0x%04lX",
+//        string, (long)keyCode, (unsigned long)flags);
+//
+//  unichar key = [string characterAtIndex:0];
+//  if((key >= 'a' && key <= 'z') || (key >= '0' && key <= '9'))
+//  { // 如果是字符则追加到写作串
+//    [self appendComposedString:string client:sender];
+//    return YES;
+//  }
+//
+//  if((keyCode == kVK_Space || keyCode == kVK_Return)
+//     && [[self composedString] length]>0)
+//  { // 如果是空格或回车且有写作串则上屏
+//    [self commitComposedString:sender];
+//    return YES;
+//  }
+//  return NO;
+//}
 
 // 该方法接收来自客户程序的按键输入，InputMethodKit会把按键事件转换成NSString发送给本方法。
 //返回YES表明输入法要处理，系统将不再把按键继续发送给应用程序；否则返回NO
